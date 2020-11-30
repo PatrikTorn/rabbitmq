4ed60c5431b0e2bfecd7db33ecd51b9ca1b3e56c
Patrik Torn <patrik.torn@outlook.com>
2020-11-30 11:52:11 +0200

    Implement code for change state from RUNNING to PAUSED

--- api/index.js
+++ api/index.js
@@ -6,6 +6,16 @@ const conString = "amqp://localhost" || "amqp://guest:guest@rabbitmq:5672";
 
 const PORT = 8081;
 
+const STATE_TOPIC = "my.s";
+
+const STATES = {
+  PAUSED: "PAUSED",
+  RUNNING: "RUNNING",
+  INIT: "INIT",
+  SHUTDOWN: "SHUTDOWN",
+};
+
+let state = STATES.RUNNING;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +26,45 @@ app.get("/messages", async (req, res) => {
   res.json(messages);
 });
 
+app.put("/state", (req, res) => {
+  const newState = req.body.payload;
+  if (!(newState in STATES) || newState === state) {
+    return res.sendStatus(400);
+  }
+  switchState(newState);
+  logs.push({
+    time: new Date(),
+    state: newState,
+  });
+  state = newState;
+  res.sendStatus(200);
+});
 
+app.get("/state", (req, res) => {
+  res.send(state);
+});
+
+function switchState(newState) {
+  switch (newState) {
+    case STATES.RUNNING:
+      amqp.connect(conString, function (error0, connection) {
+        if (error0) throw error0;
+        connection.createChannel(function (error1, channel) {
+          channel.sendToQueue(STATE_TOPIC, Buffer.from("true"));
+        });
+      });
+      break;
+    case STATES.PAUSED:
+      amqp.connect(conString, function (error0, connection) {
+        if (error0) throw error0;
+        connection.createChannel(function (error1, channel) {
+          channel.sendToQueue(STATE_TOPIC, Buffer.from("false"));
+        });
+      });
+      break;
+  }
+  console.log(`[x] API Sent ${newState}`);
+}
 module.exports = app.listen(PORT, () => {
   console.log("App listening on port", PORT);
 });
--- orig/index.js
+++ orig/index.js
@@ -1,9 +1,12 @@
 const amqp = require("amqplib/callback_api");
 const SENDING_INTERVAL_MS = 3000;
 const TOPIC = "my.o";
-const conString = "amqp://guest:guest@rabbitmq:5672";
+const STATE_TOPIC = "my.s";
+const conString = "amqp://localhost" || "amqp://guest:guest@rabbitmq:5672";
 
-amqp.connect(conString, function (error0, connection) {
+let running = true;
+
+module.exports = amqp.connect(conString, function (error0, connection) {
   if (error0) {
     throw error0;
   }
@@ -11,8 +14,13 @@ amqp.connect(conString, function (error0, connection) {
     channel.assertQueue(TOPIC, {
       durable: false,
     });
+    channel.assertQueue(STATE_TOPIC, {
+      durable: false,
+    });
+
     let i = 1;
     setInterval(() => {
+      if (!running) return;
       if (i > 3) {
         i = 1;
       }
@@ -20,5 +28,23 @@ amqp.connect(conString, function (error0, connection) {
       console.log(`[x] ORIG Sent MSG_${i}`);
       i++;
     }, SENDING_INTERVAL_MS);
+
+    channel.consume(
+      STATE_TOPIC,
+      function (msg) {
+        console.log(
+          " [x] Received %s from %s",
+          msg.content.toString(),
+          STATE_TOPIC
+        );
+        const isRunning = msg.content.toString();
+        if (isRunning === "true") {
+          running = true;
+        } else {
+          running = false;
+        }
+      },
+      { noAck: true }
+    );
   });
 });
