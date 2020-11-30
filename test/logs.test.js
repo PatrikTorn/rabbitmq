732e6a35666be9415c734a653761d83d147ed65d
Patrik Torn <patrik.torn@outlook.com>
2020-11-30 11:52:26 +0200

    Implement logs tests to API server

---
+++ test/logs.test.js
@@ -0,0 +1,19 @@
+const { assert } = require("console");
+const request = require("supertest");
+require("../orig/index.js");
+require("../imed/index.js");
+const api = require("../api/index.js");
+
+describe("api logs tests", () => {
+  it("Gets logs after putting status", (done) => {
+    setTimeout(() => {
+      request(api)
+        .get("/run-log")
+        .expect((res) => {
+          assert(res.text != "");
+          assert(res.text.startsWith(new Date().getFullYear()));
+        })
+        .expect(200, done);
+    }, 5000);
+  });
+});
